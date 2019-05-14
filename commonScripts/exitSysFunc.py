295754dfa84681d078d9950a63f584c517b995c8
jopham <48107321+jopham@users.noreply.github.com>
2019-05-14 10:39:25 -0400

    Create exitSysFunc.py

---
+++ commonScripts/exitSysFunc.py
@@ -0,0 +1,23 @@
+'''
+Purpose: Exit system if there are errors
+'''
+####################
+#	IMPORT
+####################
+
+import datetime
+
+####################
+#	EXIT FUNCTION
+####################
+def exitSysFunc(l):
+    l.write(str(datetime.datetime.now()) + " INFO: Check exitSys flag\n")
+    l.write(str(datetime.datetime.now()) + " INFO: exitSys flag = " + str(globalVars.exitSys) + "\n")
+    if globalVars.exitSys == 1:
+        if globalVars.runLocally == "1":
+            try:
+                l.write(str(datetime.datetime.now()) + ' INFO: Running locally, get AWS Keys\n')
+                expAwsKeyTime, exitSys = get_awskeytemp(l)
+            except:
+                l.write(str(datetime.datetime.now()) + ' ERROR: Cannot get aws key\n')
+                l.write(str(datetime.datetime.now()) + ' ERROR: This is required, exiting system\n')
