f0303a09f7e399c66b67bf82807ecb76e98afd73
jopham <48107321+jopham@users.noreply.github.com>
2019-05-14 11:42:20 -0400

    Update log variable name

--- main.py
+++ main.py
@@ -76,31 +76,31 @@ if not os.path.exists(os.path.dirname(logName)):
         exit()
 
 # Record start of an automation run
-l = open(logName, 'w')
-l.write('*****************************************************************************************\n')
-l.write('*****************************************************************************************\n')
-l.write('\n')
-l.write("~ Let the automation begin! ~\n")
-l.write('\n')
-l.write('*****************************************************************************************\n')
-l.write('*****************************************************************************************\n')
+auto_log = open(logName, 'w')
+auto_log.write('*****************************************************************************************\n')
+auto_log.write('*****************************************************************************************\n')
+auto_log.write('\n')
+auto_log.write("~ Let the automation begin! ~\n")
+auto_log.write('\n')
+auto_log.write('*****************************************************************************************\n')
+auto_log.write('*****************************************************************************************\n')
 
 # Record automation start time and folder and directory names
 print('Start time: ' + str(dt) + '\n')
-l.write('Start time: ' + str(dt) + '\n\n')
-l.write(str(datetime.datetime.now()) + ' INFO: Unique output folder name is ' + outputName + '\n')
+auto_log.write('Start time: ' + str(dt) + '\n\n')
+auto_log.write(str(datetime.datetime.now()) + ' INFO: Unique output folder name is ' + outputName + '\n')
 print('INFO: Unique output folder name is ' + outputName)
 
 # Create input folder for data sets
 folder = './executionLogs/' + outputName + '/input/'
 if not os.path.exists(os.path.dirname(folder)):
     try:
-        l.write(str(datetime.datetime.now()) + ' INFO: Create ' + folder + '\n')
+        auto_log.write(str(datetime.datetime.now()) + ' INFO: Create ' + folder + '\n')
         os.makedirs(os.path.dirname(folder))
-        l.write(str(datetime.datetime.now()) + ' INFO: Create ' + folder + 'dataFiles/\n')
+        auto_log.write(str(datetime.datetime.now()) + ' INFO: Create ' + folder + 'dataFiles/\n')
         os.makedirs(os.path.dirname(folder+'dataFiles/'))
     except:
-        l.write(str(datetime.datetime.now()) + ' ERROR: Creating a folder in the ' + folder + '\n')
+        auto_log.write(str(datetime.datetime.now()) + ' ERROR: Creating a folder in the ' + folder + '\n')
         print('ERROR: Issue creating a folder in the ' + folder + '\n')
         exit()
 
@@ -108,9 +108,9 @@ if not os.path.exists(os.path.dirname(folder)):
 folder = './executionLogs/' + outputName + '/output/'
 if not os.path.exists(os.path.dirname(folder)):
     try:
-        l.write(str(datetime.datetime.now()) + ' INFO: Create ' + folder + '\n')
+        auto_log.write(str(datetime.datetime.now()) + ' INFO: Create ' + folder + '\n')
         os.makedirs(os.path.dirname(folder))
     except:
-        l.write(str(datetime.datetime.now()) + ' ERROR: Creating ' + folder + '\n')
+        auto_log.write(str(datetime.datetime.now()) + ' ERROR: Creating ' + folder + '\n')
         print('ERROR: Issue creating ' + folder + '\n')
         exit()
