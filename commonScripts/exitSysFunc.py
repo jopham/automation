db4d4cedb6ead8de172640b381f66fad1311ae10
jopham <48107321+jopham@users.noreply.github.com>
2019-05-14 11:40:29 -0400

    Update log variable name

--- commonScripts/exitSysFunc.py
+++ commonScripts/exitSysFunc.py
@@ -10,15 +10,15 @@ import datetime
 ####################
 #	EXIT FUNCTION
 ####################
-def exitSysFunc(l):
+def exitSysFunc(auto_log):
     """Exits system if there are errors"""
-    l.write(str(datetime.datetime.now()) + " INFO: Check exitSys flag\n")
-    l.write(str(datetime.datetime.now()) + " INFO: exitSys flag = " + str(globalVars.exitSys) + "\n")
+    auto_log.write(str(datetime.datetime.now()) + " INFO: Check exitSys flag\n")
+    auto_log.write(str(datetime.datetime.now()) + " INFO: exitSys flag = " + str(globalVars.exitSys) + "\n")
     if globalVars.exitSys == 1:
         if globalVars.runLocally == "1":
             try:
-                l.write(str(datetime.datetime.now()) + ' INFO: Running locally, get AWS Keys\n')
+                auto_log.write(str(datetime.datetime.now()) + ' INFO: Running locally, get AWS Keys\n')
                 expAwsKeyTime, exitSys = get_awskeytemp(l)
             except:
-                l.write(str(datetime.datetime.now()) + ' ERROR: Cannot get aws key\n')
-                l.write(str(datetime.datetime.now()) + ' ERROR: This is required, exiting system\n')
+                auto_log.write(str(datetime.datetime.now()) + ' ERROR: Cannot get aws key\n')
+                auto_log.write(str(datetime.datetime.now()) + ' ERROR: This is required, exiting system\n')
