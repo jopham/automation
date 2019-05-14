df5e67f338db88e04bf195e2dab031315d77e96c
jopham <48107321+jopham@users.noreply.github.com>
2019-05-14 11:41:05 -0400

    Update log variable name

--- commonScripts/globalVars.py
+++ commonScripts/globalVars.py
@@ -12,13 +12,13 @@ def init(startDt, outputNm, exit, runLocally):
     """Creates global variables: startDate, outputName, exitSys, runLocal"""
     global startDate
     startDate = startDt
-    l.write(str(datetime.datetime.now()) + ' INFO: Global variable startDate set as ' + str(startDate) + '\n')
+    auto_log.write(str(datetime.datetime.now()) + ' INFO: Global variable startDate set as ' + str(startDate) + '\n')
     global outputName
     outputName = outputNm
-    l.write(str(datetime.datetime.now()) + ' INFO: Global variable outputName set as ' + outputName + '\n')
+    auto_log.write(str(datetime.datetime.now()) + ' INFO: Global variable outputName set as ' + outputName + '\n')
     global exitSys
     exitSys = exit
-    l.write(str(datetime.datetime.now()) + ' INFO: Global variable exitSys set as ' + str(exitSys) + '\n')
+    auto_log.write(str(datetime.datetime.now()) + ' INFO: Global variable exitSys set as ' + str(exitSys) + '\n')
     global runLocally
     runLocally = runLocal
-    l.write(str(datetime.datetime.now()) + ' INFO: Global variable runLocally set as ' + runLocally + '\n')
+    auto_log.write(str(datetime.datetime.now()) + ' INFO: Global variable runLocally set as ' + runLocally + '\n')
