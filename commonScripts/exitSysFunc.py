e80df2757a65447725a46994da500538fdba72c8
jopham <48107321+jopham@users.noreply.github.com>
2019-05-19 15:50:15 -0400

    Update exitSysFunc.py

--- commonScripts/exitSysFunc.py
+++ commonScripts/exitSysFunc.py
@@ -1,15 +1,10 @@
 """
 Purpose: Exits system if there are errors
 """
-####################
-#	IMPORT
-####################
 
 import datetime
 
-####################
-#	EXIT FUNCTION
-####################
+
 def exitSysFunc(auto_log):
     """Exits system if there are errors"""
     auto_log.write(str(datetime.datetime.now()) + " INFO: Check exitSys flag\n")
@@ -22,3 +17,24 @@ def exitSysFunc(auto_log):
             except:
                 auto_log.write(str(datetime.datetime.now()) + ' ERROR: Cannot get aws key\n')
                 auto_log.write(str(datetime.datetime.now()) + ' ERROR: This is required, exiting system\n')
+        end = datetime.datetime.now()
+
+        auto_log.write(str(datetime.datetime.now()) + " INFO: End time = " + str(end) + "\n")
+        print("End time: "+str(end)+"\n")
+
+        # Get the time elapsed and convert it to seconds
+        elapsedTimeSecsOG = (end - globalVars.startDate).seconds
+
+        # Get the number of whole hours elapsed
+        elapsedTimeHours = elapsedTimeSecsOG // 3600
+
+        # Subtract the number of whole hours, get the number of whole minutes elapsed
+        elapsedTimeMins = (elapsedTimeSecsOG % 3600) // 60
+
+        # Get the number of seconds passed after subtracting the whole number of hours and minutes
+        elapsedTimeSecs = elapsedTimeSecsOG % 60
+
+        runTimeStr = "Run time: " + str(elapsedTimeHours) + " hours, " + str(elapsedTimeMins) + " mins, and " + str(elapsedTimeSecs) + " secs\n"
+        auto_log.write(str(datetime.datetime.now()) + " INFO: Run Time = " + runTimeStr)
+        print("Run Time = " + runTimeStr)
+        auto_log.close()
